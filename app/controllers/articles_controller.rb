504b5ce2c34c7c8cecc02d4ac1d6d49f349b4d23
EnasGaberMostafa <enassays2@gmail.com>
2017-09-17 15:51:30 +0200

    extract redundency

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -9,8 +11,7 @@ class ArticlesController < ApplicationController
 
 
     def show 
-        @article = Article.find(params[:id])
-   end
+    end
 
     def create
         # render plain: params[:article].inspect
@@ -25,11 +26,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit 
-        @article = Article.find(params[:id]) 
     end
 
     def update 
-        @article = Article.find(params[:id])
         if @article.update(articles_params)
             flash[:notice] = "Article was successfully updated" # showed in appplication view
             redirect_to article_path(@article)   #show
@@ -39,7 +38,6 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -48,5 +46,8 @@ class ArticlesController < ApplicationController
     private
     def articles_params
         params.require(:article).permit(:title, :description)
+    end
+    def set_article
+        @article = Article.find(params[:id])         
     end
 end
